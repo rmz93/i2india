1e470e1f9952b1d102db9ec6ce9b9e339130c119
rmz93 <rameez2612@gmail.com>
2014-06-02 14:47:55 +0530

    angular set up

---
+++ site/app/scripts/controllers/home.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('siteApp')
+  .controller('HomeCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
